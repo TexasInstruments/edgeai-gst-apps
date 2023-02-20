982740d5e3577a9bb3aebfbd546b26a6dbbfe336
Abhay Chirania <a-chirania@ti.com>
2023-02-20 17:23:57 +0530

    optiflow: Fix issue with multifilesrc and kmssink
    kmssink use sync=true only in case of image inputs.
    
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- scripts/optiflow/config_parser.py
+++ scripts/optiflow/config_parser.py
@@ -157,7 +157,6 @@ class Output:
         self.subflows.append(subflow)
         if (self.mosaic):
             self.gst_mosaic_str = self.gst_mosaic_str + \
-                         'src::pool-size=3 ' + \
                          'sink_%d::startx="<%d>"  ' % (disp_id, subflow.x_pos) + \
                          'sink_%d::starty="<%d>"  ' % (disp_id, subflow.y_pos) + \
                          'sink_%d::widths="<%d>"   ' % (disp_id, subflow.width) + \
