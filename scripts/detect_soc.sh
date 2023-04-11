bc5346030d82baf7ffca9fa29df5fb8909471269
Rahul T R <r-ravikumar@ti.com>
2023-04-11 15:00:27 +0530

    scripts: detect_soc: Set DEVICE_NAME env with part numbers
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- scripts/detect_soc.sh
+++ scripts/detect_soc.sh
@@ -2,18 +2,23 @@
 if grep -q j721e /proc/device-tree/compatible
 then
     export SOC=j721e
+    export DEVICE_NAME=TDA4VM
 elif grep -q j721s2 /proc/device-tree/compatible
 then
     export SOC=j721s2
+    export DEVICE_NAME=AM68A
 elif grep -q j784s4 /proc/device-tree/compatible
 then
     export SOC=j784s4
+    export DEVICE_NAME=AM69A
 elif grep -q am625 /proc/device-tree/compatible
 then
     export SOC=am62
+    export DEVICE_NAME=AM62
 elif grep -q am62a /proc/device-tree/compatible
 then
     export SOC=am62a
+    export DEVICE_NAME=AM62A
 else
     echo "WARNING: EdgeAI Apps is not supported in this Target"
 fi
