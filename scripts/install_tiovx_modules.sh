4ca7795edab303baa0f6f11c7403bea12205b779
Abhay Chirania <a-chirania@ti.com>
2023-04-17 16:04:15 +0530

    scripts: Add script to install edgeai-apps-utils
    Also update all repo command to pull from develop
    
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- scripts/install_tiovx_modules.sh
+++ scripts/install_tiovx_modules.sh
@@ -65,7 +65,7 @@ done
 cd $install_dir
 ls | grep "edgeai-tiovx-modules"
 if [ "$?" -ne "0" ]; then
-    git clone --single-branch --branch EDGEAI_APP_STACK_08_06_00_02 https://git.ti.com/cgit/edgeai/edgeai-tiovx-modules
+    git clone --single-branch --branch develop https://git.ti.com/cgit/edgeai/edgeai-tiovx-modules
     if [ "$?" -ne "0" ]; then
         cd $current_dir
         exit 1
