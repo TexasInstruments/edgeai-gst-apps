1bddbb5a75aff304c9d83f13fa9c88c9ca51b722
JuneChul Roh <jroh@ti.com>
2023-07-11 12:52:18 -0500

    [gpio libs] Updated the tags to 'release-1.1.0' for ti-gpio-py and ti-gpio-cpp.

--- scripts/install_ti_gpio_libs.sh
+++ scripts/install_ti_gpio_libs.sh
@@ -66,7 +66,7 @@ cd $install_dir
 ls | grep "ti-gpio-py"
 if [ "$?" -ne "0" ]; then
     echo "Cloning TI.GPIO python project."
-    git clone --single-branch --branch release-1.0.0 https://github.com/TexasInstruments/ti-gpio-py.git
+    git clone --single-branch --branch release-1.1.0 https://github.com/TexasInstruments/ti-gpio-py.git
     if [ "$?" -ne "0" ]; then
         cd $current_dir
         exit 1
@@ -106,7 +106,7 @@ fi
 ls | grep "ti-gpio-cpp"
 if [ "$?" -ne "0" ]; then
     echo "Cloning TI GPIO CPP project."
-    git clone --single-branch --branch release-1.0.0 https://github.com/TexasInstruments/ti-gpio-cpp.git
+    git clone --single-branch --branch release-1.1.0 https://github.com/TexasInstruments/ti-gpio-cpp.git
     if [ "$?" -ne "0" ]; then
         cd $current_dir
         exit 1
