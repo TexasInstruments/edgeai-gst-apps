06c1e30a0bfec543b5df9a56c3434ad74021a019
Abhay Chirania <a-chirania@ti.com>
2023-10-19 19:37:43 +0530

    init_script: Change SDK Version for 9.1
    Also update git tag in install scripts
    
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- init_script.sh
+++ init_script.sh
@@ -73,5 +73,5 @@ export EDGEAI_GST_APPS_PATH=/opt/edgeai-gst-apps
 export EDGEAI_DATA_PATH=/opt/edgeai-test-data
 export OOB_DEMO_ASSETS_PATH=/opt/oob-demo-assets
 export MODEL_ZOO_PATH=/opt/model_zoo
-export EDGEAI_VERSION=9.0
-export EDGEAI_SDK_VERSION=09_00_00
+export EDGEAI_VERSION=9.1
+export EDGEAI_SDK_VERSION=09_01_00
--- scripts/install_gst_plugins.sh
+++ scripts/install_gst_plugins.sh
@@ -39,7 +39,7 @@ else
     install_dir="../../"
 fi
 
-branch_name=EDGEAI_APP_STACK_09_00_00_00
+branch_name=EDGEAI_APP_STACK_09_01_00_00
 
 while getopts ":i:b:d" flag; do
     case "${flag}" in
